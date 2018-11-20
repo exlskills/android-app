d97c43f30f44201d848823dead82c675aef19238
Sasha Varlamov <sasha@sashavarlamov.com>
2018-11-20 08:03:06 -0800

    Demo cards

---
+++ app/src/main/java/com/exlskills/android/CardRecyclerAdapter.kt
@@ -0,0 +1,57 @@
+package com.exlskills.android
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+class CardRecyclerAdapter : RecyclerView.Adapter<CardRecyclerAdapter.ViewHolder>() {
+
+    private val titles = arrayOf("Chapter One",
+        "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven",
+        "Chapter Eight")
+
+    private val details = arrayOf("Item one details", "Item two details",
+        "Item three details", "Item four details",
+        "Item file details", "Item six details",
+        "Item seven details", "Item eight details")
+
+    private val images = intArrayOf(R.drawable.gophergif,
+        R.drawable.gophergif, R.drawable.gophergif,
+        R.drawable.gophergif, R.drawable.gophergif,
+        R.drawable.gophergif, R.drawable.gophergif,
+        R.drawable.gophergif)
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemDetail.text = details[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+}
--- app/src/main/java/com/exlskills/android/MainActivity.kt
+++ app/src/main/java/com/exlskills/android/MainActivity.kt
@@ -3,15 +3,20 @@ package com.exlskills.android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.Group
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.exlskills.android.com.exlskills.android.remote.CourseLiteMeta
 import com.exlskills.android.com.exlskills.android.remote.Graph
 import com.exlskills.android.com.exlskills.android.remote.GraphCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val gqlApi = Graph()
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<CardRecyclerAdapter.ViewHolder>? = null
     private lateinit var textField: TextView
     private lateinit var progressBar: ProgressBar
     private lateinit var courseListGroup: Group
@@ -24,9 +29,18 @@ class MainActivity : AppCompatActivity() {
         this.textField = findViewById(R.id.foobartextview)
         this.progressBar = findViewById(R.id.foobarprogview)
         this.courseListGroup = findViewById(R.id.courseListGroup)
-        val self = this
+        layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
 
-        gqlApi.getAllCourses("relevant", object: GraphCallback<List<CourseLiteMeta>> {
+        adapter = CardRecyclerAdapter()
+        recycler_view.adapter = adapter
+
+        loadData()
+    }
+
+    fun loadData() {
+        val self = this
+        gqlApi.getAllCourses("mine", object: GraphCallback<List<CourseLiteMeta>> {
             override fun onFailure(msg: String, code: Int) {
                 println("onfailure")
             }
@@ -41,10 +55,5 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         })
-        changeText()
-    }
-
-    fun changeText() {
-        textField.setText("Foobar");
     }
 }
