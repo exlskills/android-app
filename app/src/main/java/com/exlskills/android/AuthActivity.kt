3615d3b405614b393b00a6ecf80706c9fac3e568
Sasha Varlamov <sasha@sashavarlamov.com>
2018-11-19 21:23:33 -0800

    Scaffold first screens, setup demo bottom nav for home

---
+++ app/src/main/java/com/exlskills/android/AuthActivity.kt
@@ -0,0 +1,18 @@
+package com.exlskills.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.exlskills.android.com.exlskills.android.remote.Graph
+
+class AuthActivity : AppCompatActivity() {
+    private val gqlApi = Graph()
+    // TODO check if user logged in:
+    //      Logged in: send to MainActivity
+    //      Not:       send to the LoginActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+
+}
---
+++ app/src/main/java/com/exlskills/android/SplashActivity.kt
@@ -0,0 +1,52 @@
+package com.exlskills.android
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.exlskills.android.com.exlskills.android.remote.Graph
+
+class SplashActivity : AppCompatActivity() {
+    private val gqlApi = Graph()
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    internal val mRunnable: Runnable = Runnable {
+        // TODO check for auth
+        // TODO If no auth, then send to auth: val intent = Intent(applicationContext, MainActivity::class.java)
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Initializing the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+}
